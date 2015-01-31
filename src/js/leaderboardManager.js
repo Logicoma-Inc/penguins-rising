263c6c60c9c7e53f0e7bb9bb946591d401ec3c4c
Anthony Fassett <fassetar@gmail.com>
2015-01-31 01:12:07 -0500

    Sweet looking javascript.

--- src/js/leaderboardManager.js
+++ src/js/leaderboardManager.js
@@ -1,5 +1,3 @@
-"use strict";
-
 var leadManager = leadManager || {};
 leadManager.preloaded = false;
 leadManager.leaderboards = {};
@@ -7,115 +5,116 @@ leadManager.leaderboards = {};
 /* loads up the leaderboard definitions. Not the scores themselves */
 leadManager.preloadData = function() {
 
-  var request = gapi.client.games.leaderboards.list();
-  request.execute(function(response) {
-    console.log('Leaderboard data', response);
-    if (response.kind == 'games#leaderboardListResponse' &&
-        response.hasOwnProperty('items')) {
-      for (var i =0; i<response.items.length; i++) {
-        leadManager.leaderboards[response.items[i].id] = response.items[i];
-      }
-    }
-    leadManager.preloaded = true;
-    welcome.dataLoaded(welcome.ENUM_LEADERBOARDS);
-  });
-};
-
-leadManager.getLeaderboardObject = function(leadId)
-{
-  return leadManager.leaderboards[leadId];
-};
-
-
-
-leadManager.gotScore = function(receivedScore, difficulty, callback)
-{
-  var leaderboardId = (difficulty == game.EASY) ? constants.LEADERBOARD_EASY : constants.LEADERBOARD_HARD;
-
-  var request = gapi.client.games.scores.submit(
-      {leaderboardId: leaderboardId,
-      score: receivedScore}
-  );
-  request.execute(function(response) {
-    console.log('Data from submitting high score is ', response);
-    var newWeeklyHighScore = false;
-    if (response.hasOwnProperty('beatenScoreTimeSpans')) {
-      for (var i=0; i<response.beatenScoreTimeSpans.length; i++) {
-        if (response.beatenScoreTimeSpans[i] == 'WEEKLY') {
-          console.log('Hooray! New weekly high score!');
-          newWeeklyHighScore = true;
-          leaderboardWidget.show(leaderboardId);
-          //TODO: Update our internal model as well
-        } else {
+    var request = gapi.client.games.leaderboards.list();
+    request.execute(function(response) {
+        console.log('Leaderboard data', response);
+        if (response.kind === 'games#leaderboardListResponse' &&
+            response.hasOwnProperty('items')) {
+            for (var i = 0; i < response.items.length; i++) {
+                leadManager.leaderboards[response.items[i].id] = response.items[i];
+            }
         }
-      }
-    }
-    callback(newWeeklyHighScore);
-  });
+        leadManager.preloaded = true;
+        welcome.dataLoaded(welcome.ENUM_LEADERBOARDS);
+    });
+};
+
+leadManager.getLeaderboardObject = function(leadId) {
+    return leadManager.leaderboards[leadId];
+};
+
+leadManager.gotScore = function(receivedScore, difficulty, callback) {
+    var leaderboardId = (difficulty === game.EASY) ? constants.LEADERBOARD_EASY : constants.LEADERBOARD_HARD;
+
+    var request = gapi.client.games.scores.submit({
+        leaderboardId: leaderboardId,
+        score: receivedScore
+    });
+    request.execute(function(response) {
+        console.log('Data from submitting high score is ', response);
+        var newWeeklyHighScore = false;
+        if (response.hasOwnProperty('beatenScoreTimeSpans')) {
+            for (var i = 0; i < response.beatenScoreTimeSpans.length; i++) {
+                if (response.beatenScoreTimeSpans[i] === 'WEEKLY') {
+                    console.log('Hooray! New weekly high score!');
+                    newWeeklyHighScore = true;
+                    leaderboardWidget.show(leaderboardId);
+                    //TODO: Update our internal model as well
+                } else {}
+            }
+        }
+        callback(newWeeklyHighScore);
+    });
 };
 var leaderboardWidget = leaderboardWidget || {};
 
-leaderboardWidget.show = function(leadId)
-{
-  var leaderboardObject = leadManager.getLeaderboardObject(leadId);
+leaderboardWidget.show = function(leadId) {
+    var leaderboardObject = leadManager.getLeaderboardObject(leadId);
 
 
-  // Let's populate the little widget
-  var iconUrl =   (leaderboardObject.iconUrl) ? leaderboardObject.iconUrl : 'img/genericLeaderboard.png';
-  $('#leadUnlocked img').prop('src', iconUrl);
-  $('#leadUnlocked #leadName').text(leaderboardObject.name);
-  $('#leadUnlocked').css({top: '250px', opacity: '1.0'});
-  $('#leadUnlocked').show();
-  $('#leadUnlocked').delay(3000).animate({top: '50px', opacity: 0.1}, 500, function() {
-    $('#leadUnlocked').hide();
-  });
+    // Let's populate the little widget
+    var iconUrl = (leaderboardObject.iconUrl) ? leaderboardObject.iconUrl : 'img/genericLeaderboard.png';
+    $('#leadUnlocked img').prop('src', iconUrl);
+    $('#leadUnlocked #leadName').text(leaderboardObject.name);
+    $('#leadUnlocked').css({
+        top: '250px',
+        opacity: '1.0'
+    });
+    $('#leadUnlocked').show();
+    $('#leadUnlocked').delay(3000).animate({
+        top: '50px',
+        opacity: 0.1
+    }, 500, function() {
+        $('#leadUnlocked').hide();
+    });
 
 };
 var leaderboardsTable = leaderboardsTable || {};
 
 leaderboardsTable.showAllLeaderboards = function() {
-  leaderboardsTable.clearOut();
-  if (leadManager.preloaded) {
-    $.each(leadManager.leaderboards, function(id, leadObject) {
-      var $leaderboardRow = leaderboardsTable.buildLeaderboardsRowFromData(leadObject);
-      $leaderboardRow.appendTo($('#leaderboardsTable tbody'));
-    });
-    $('#leaderboards').fadeIn();
-  }
-  $('#pageHeader').text('Leaderboards');
+    leaderboardsTable.clearOut();
+    if (leadManager.preloaded) {
+        $.each(leadManager.leaderboards, function(id, leadObject) {
+            var $leaderboardRow = leaderboardsTable.buildLeaderboardsRowFromData(leadObject);
+            $leaderboardRow.appendTo($('#leaderboardsTable tbody'));
+        });
+        $('#leaderboards').fadeIn();
+    }
+    $('#pageHeader').text('Leaderboards');
 };
 
 leaderboardsTable.buildLeaderboardsRowFromData = function(leadObj) {
-  var $tableRow = $('<tr></tr>');
-  var leaderboardIcon = (leadObj.iconUrl) ? leadObj.iconUrl : 'img/genericLeaderboard.png';
-  var $iconCell = $('<td></td>')
-      .append($('<img />')
-      .prop('src', leaderboardIcon).addClass('medIcon')
-  );
-  var $nameCell = $('<td></td>').text(leadObj.name);
-  var $viewButtonCell = $('<td></td>')
-      .append($('<input type="button" />')
-      .prop('value', 'View')
-      .click(function() {leaderboardsTable.selectLeaderboard(leadObj.id)})
-  );
-  $tableRow.append($iconCell).append($nameCell).append($viewButtonCell);
+    var $tableRow = $('<tr></tr>');
+    var leaderboardIcon = (leadObj.iconUrl) ? leadObj.iconUrl : 'img/genericLeaderboard.png';
+    var $iconCell = $('<td></td>')
+        .append($('<img />')
+            .prop('src', leaderboardIcon).addClass('medIcon')
+        );
+    var $nameCell = $('<td></td>').text(leadObj.name);
+    var $viewButtonCell = $('<td></td>')
+        .append($('<input type="button" />')
+            .prop('value', 'View')
+            .click(function() {
+                leaderboardsTable.selectLeaderboard(leadObj.id);
+            })
+        );
+    $tableRow.append($iconCell).append($nameCell).append($viewButtonCell);
 
-  return $tableRow;
+    return $tableRow;
 };
 
-leaderboardsTable.selectLeaderboard = function(leaderboardId)
-{
-  $('#leaderboards').hide();
-  leaderboardTable.showLeaderboard(leaderboardId, leaderboardTable.BACK_TO_ALL_LEADERBOARDS);
+leaderboardsTable.selectLeaderboard = function(leaderboardId) {
+    $('#leaderboards').hide();
+    leaderboardTable.showLeaderboard(leaderboardId, leaderboardTable.BACK_TO_ALL_LEADERBOARDS);
 
 };
 
 leaderboardsTable.clearOut = function() {
-  $('#leaderboardsTable tbody').html('');
+    $('#leaderboardsTable tbody').html('');
 };
 
 leaderboardsTable.goBack = function() {
-  $('#leaderboards').fadeOut();
-  welcome.loadUp();
+    $('#leaderboards').fadeOut();
+    welcome.loadUp();
 
 };
--- src/js/startup.js
+++ src/js/startup.js
@@ -1,65 +1,58 @@
-(function () {
-	var signinCallback = function (auth) {
-		login.handleAuthResult(auth)
-	}
-	var apisLoaded = function () {
-		login.init();
-	};
-	function AssetManager() {
-		this.successCount = 0;
-		this.errorCount = 0;
-		this.downloadQueue = [];
-		this.cache = {};
-	}
-	AssetManager.prototype.queueDownload = function (path) {
-		this.downloadQueue.push(path);
-	}
-	AssetManager.prototype.downloadAll = function (downloadCallback) {
-		if (this.downloadQueue.length === 0) {
-			downloadCallback();
-		}
-		for (var i = 0; i < this.downloadQueue.length; i++) {
-			var path = this.downloadQueue[i];
-			var img = new Image();
-			var that = this;
-			img.addEventListener("load", function () {
-				console.log(this.src + ' is loaded');
-				that.successCount += 1;
-				if (that.isDone()) {
-					downloadCallback();
-				}
-			}, false);
-			img.addEventListener("error", function () {
-				that.errorCount += 1;
-				if (that.isDone()) {
-					downloadCallback();
-				}
-			}, false);
-			img.src = path;
-			this.cache[path] = img;
-		}
-	}
-	AssetManager.prototype.isDone = function () {
-		return (this.downloadQueue.length == this.successCount + this.errorCount);
-	}
-	AssetManager.prototype.getAsset = function (path) {
-		return this.cache[path];
-	}
-	var ASSET_MANAGER = new AssetManager();
-	//ASSET_MANAGER.queueDownload('images/landscape.png');
-	ASSET_MANAGER.downloadAll(function(){});
-	if (ASSET_MANAGER.isDone)
-	{
-		document.getElementById("Gamer").innerHTML = "Play";
-	}
-	
-	(function (i, s, o, g, r, a, m) {
-	i['GoogleAnalyticsObject'] = r; i[r] = i[r] || function () {
-		(i[r].q = i[r].q || []).push(arguments)
-	}, i[r].l = 1 * new Date(); a = s.createElement(o),
-	m = s.getElementsByTagName(o)[0]; a.async = 1; a.src = g; m.parentNode.insertBefore(a, m)
-})(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+(function() {
+    "use strict";
+    var signinCallback = function(auth) {
+        login.handleAuthResult(auth);
+    };
+    var apisLoaded = function() {
+        login.init();
+    };
 
-ga('create', 'UA-41648493-1', 'penguinsontherise.appspot.com');
-ga('send', 'pageview');
+    function AssetManager() {
+        this.successCount = 0;
+        this.errorCount = 0;
+        this.downloadQueue = [];
+        this.cache = {};
+    }
+    AssetManager.prototype.queueDownload = function(path) {
+        this.downloadQueue.push(path);
+    };
+    AssetManager.prototype.downloadAll = function(downloadCallback) {
+        if (this.downloadQueue.length === 0) {
+            downloadCallback();
+        }
+        for (var i = 0; i < this.downloadQueue.length; i++) {
+            var path = this.downloadQueue[i];
+            var img = new Image();
+            var that = this;
+
+            img.src = path;
+            this.cache[path] = img;
+        }
+    };
+    AssetManager.prototype.isDone = function() {
+        return (this.downloadQueue.length === this.successCount + this.errorCount);
+    };
+    AssetManager.prototype.getAsset = function(path) {
+        return this.cache[path];
+    };
+    var ASSET_MANAGER = new AssetManager();
+    ASSET_MANAGER.downloadAll(function() {});
+    if (ASSET_MANAGER.isDone) {
+        document.getElementById("Gamer").innerHTML = "Play";
+    }
+
+    (function(i, s, o, g, r, a, m) {
+        i['GoogleAnalyticsObject'] = r;
+        i[r] = i[r] || function() {
+            (i[r].q = i[r].q || []).push(arguments);
+        }, i[r].l = 1 * new Date();
+        a = s.createElement(o),
+            m = s.getElementsByTagName(o)[0];
+        a.async = 1;
+        a.src = g;
+        m.parentNode.insertBefore(a, m);
+    })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+
+    ga('create', 'UA-41648493-1', 'penguinsontherise.appspot.com');
+    ga('send', 'pageview');
 })();
